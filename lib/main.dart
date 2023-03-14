a6f75559c8babd099c317e3c35eb550d018bc34d
Preisler <preisler.andras@gmail.com>
2023-03-14 20:52:03 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const PetrikApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  static const String _title = 'Petrik';
+class PetrikApp extends StatelessWidget {
+  const PetrikApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
+      title: 'Petrik App',
+      home: const Scaffold(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
+class RoorPage extends StatefulWidget {
+  const RoorPage({super.key});
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<RoorPage> createState() => _RoorPageState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: IKSZ',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Óra rend',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Helyettesítés',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class _RoorPageState extends State<RoorPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Petrik'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper),
-            label: 'Petrik News',
-            backgroundColor: Color.fromARGB(255, 54, 244, 219),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'IKSZ',
-            backgroundColor: Color.fromARGB(255, 54, 244, 219),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.alarm_rounded),
-            label: 'Óra rend',
-            backgroundColor: Color.fromARGB(255, 54, 244, 219),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
-            label: 'Helyettesítés',
-            backgroundColor: Color.fromARGB(255, 54, 244, 219),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
+    return Container();
   }
 }
