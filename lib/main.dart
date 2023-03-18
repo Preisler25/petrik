758795165b1c15971cf6c002a928991d9ec90edf
Preisler <preisler.andras@gmail.com>
2023-03-18 11:48:52 +0100

    finaly working

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,153 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const PetrikApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class PetrikApp extends StatelessWidget {
-  const PetrikApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static final List<Widget> _widgetOptions = <Widget>[
+    Page1(),
+    Page2(),
+    Page3(),
+    Page4(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Petrik',
       debugShowCheckedModeBanner: false,
-      title: 'Petrik App',
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        primarySwatch: Colors.blue,
       ),
-      home: const RootPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Petrik news'),
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.newspaper),
+              label: 'News',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.work),
+              label: 'IKSZ',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: 'Órarend',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Helyettesítés',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.blue,
+        ),
+      ),
     );
   }
 }
 
-class RootPage extends StatefulWidget {
-  const RootPage({super.key});
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
 
-  @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Petrik news'),
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text(
+          'Page 1',
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+          ),
         ),
-        body: const Center(
-          child: Text('Hello world'),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Center(
+        child: Text(
+          'Page 2',
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+          ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'News',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Iksz',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.school), label: 'Helyettesítés'),
-            BottomNavigationBarItem(icon: Icon(Icons.school), label: 'Órarend'),
-          ],
-          currentIndex: 0,
-          selectedItemColor: Colors.amber[800],
-        ));
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      child: Center(
+        child: Text(
+          'Page 3',
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Page4 extends StatelessWidget {
+  const Page4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'Page 4',
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
