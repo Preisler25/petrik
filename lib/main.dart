8e0c2ee8758f03bcbd45d279ed14696539e19720
Preisler <preisler.andras@gmail.com>
2023-03-19 22:24:38 +0100

    main trying to make the pop up for IKSZ

--- lib/main.dart
+++ lib/main.dart
@@ -206,6 +206,10 @@ class Post extends StatelessWidget {
         openBuilder: (context, action) {
           return Container(
             color: Colors.white,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(12),
+            ),
             child: Center(
               child: Text(
                 title,
