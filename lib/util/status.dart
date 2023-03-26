77bb8bd4988ff25b0056420d1217ac5433696490
Preisler <preisler.andras@gmail.com>
2023-03-26 13:28:49 +0200

    main

--- lib/util/status.dart
+++ lib/util/status.dart
@@ -1,15 +1,16 @@
-import 'dart:ffi';
-
 class Status {
   final bool status;
+  final String auth_token;
 
   const Status({
     required this.status,
+    required this.auth_token,
   });
 
   factory Status.fromJson(Map<dynamic, dynamic> json) {
     return Status(
       status: json['status'],
+      auth_token: json['auth_token'],
     );
   }
 }
