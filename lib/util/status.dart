5f3bedb91d4b4d1b38864aae3b8465316bcf0f87
Preisler <preisler.andras@gmail.com>
2023-03-26 17:14:54 +0200

    main

--- lib/util/status.dart
+++ lib/util/status.dart
@@ -1,16 +1,21 @@
+import 'package:petrik/util/user.dart';
+
 class Status {
   final bool status;
   final String auth;
+  final User user;
 
   const Status({
     required this.status,
     required this.auth,
+    required this.user,
   });
 
   factory Status.fromJson(Map<dynamic, dynamic> json) {
     return Status(
       auth: json['auth'],
       status: json['status'],
+      user: User.fromJson(json['user']),
     );
   }
 }
--- lib/util/user.dart
+++ lib/util/user.dart
@@ -2,11 +2,13 @@ class User {
   final String name;
   final String password;
   final String osztaly;
+  String auth;
 
-  const User({
+  User({
     required this.name,
     required this.password,
     required this.osztaly,
+    this.auth = '',
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
