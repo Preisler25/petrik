5a8eef890bfeaf64bfbf92f4a29d6e047ab6a1ef
Preisler <preisler.andras@gmail.com>
2023-03-26 17:48:39 +0200

    working

--- lib/util/status.dart
+++ lib/util/status.dart
@@ -2,18 +2,15 @@ import 'package:petrik/util/user.dart';
 
 class Status {
   final bool status;
-  final String auth;
-  final User user;
+  final User? user;
 
   const Status({
     required this.status,
-    required this.auth,
-    required this.user,
+    this.user,
   });
 
   factory Status.fromJson(Map<dynamic, dynamic> json) {
     return Status(
-      auth: json['auth'],
       status: json['status'],
       user: User.fromJson(json['user']),
     );
--- lib/util/user.dart
+++ lib/util/user.dart
@@ -1,26 +1,21 @@
 class User {
   final String name;
-  final String password;
   final String osztaly;
-  String auth;
 
   User({
     required this.name,
-    required this.password,
     required this.osztaly,
-    this.auth = '',
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       name: json['name'],
-      password: json['password'],
       osztaly: json['osztaly'],
     );
   }
 
   @override
   String toString() {
-    return 'name: $name, password: $password';
+    return 'name: $name, osztaly: $osztaly';
   }
 }
