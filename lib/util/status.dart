382aca270c15b6b705579a73a5821adfbc063835
Preisler <preisler.andras@gmail.com>
2023-03-26 16:30:55 +0200

    main

--- lib/util/status.dart
+++ lib/util/status.dart
@@ -1,16 +1,13 @@
 class Status {
   final bool status;
-  final String authToken;
 
   const Status({
     required this.status,
-    required this.authToken,
   });
 
   factory Status.fromJson(Map<dynamic, dynamic> json) {
     return Status(
       status: json['status'],
-      authToken: json['auth_token'],
     );
   }
 }
--- lib/util/user.dart
+++ lib/util/user.dart
@@ -23,6 +23,30 @@ class User {
     );
   }
 
+  Future<Status> regUser() async {
+    final response = await http.post(
+      Uri.parse('${AppConstants.API_REGISTER}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': name,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      debugPrint(response.body);
+      return Status.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to register user');
+    }
+  }
+
   Future<Status> checkUser() async {
     final response = await http.get(
         Uri.parse('${AppConstants.API_LOGIN}?name=$name&password=$password'));
