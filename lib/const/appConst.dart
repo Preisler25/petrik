95915aa2fbacce7ee5622ab0a39e90d52774d058
Preisler <preisler.andras@gmail.com>
2023-03-26 04:16:13 +0200

    main

--- lib/const/appConst.dart
+++ lib/const/appConst.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String Base_URL = 'http://192.168.1.199:80';
+  static const String Base_URL = '192.168.1.114:80';
   static const String API_URL = '$Base_URL/api';
   static const String API_LOGIN = '$API_URL/login';
   static const String API_IKSZ = '$API_URL/iksz';
