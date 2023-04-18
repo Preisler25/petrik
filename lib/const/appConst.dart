b3ade9a7eaa190fc361b857cf6c7753f9250dd54
Preisler <preisler.andras@gmail.com>
2023-04-18 23:10:19 +0200

    mergee

--- lib/const/appConst.dart
+++ lib/const/appConst.dart
@@ -1,12 +1,7 @@
 class AppConstants {
-<<<<<<< HEAD
   //static const String Base_URL = 'https://helyettesites.petrik.hu/'; //pord
   static const String Base_URL = 'http://192.168.1.199:80'; //Preisler
   //static const String Base_URL = 'http://192.168.64.202:80'; //preisle mac
-=======
-  //static const String Base_URL = 'http://192.168.1.199:80'; //Preisler
-  static const String Base_URL = 'http://192.168.84.202:80'; //preisle mac
->>>>>>> 7c57043dc05ee9652f43a1e195e05a3d25007160
   //static const String Base_URL = 'http://192.168.100.14:80'; //Mate
   static const String API_URL = '$Base_URL/api';
   static const String API_LOGIN = '$API_URL/login';
