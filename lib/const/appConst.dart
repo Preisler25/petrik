5895ceff1b8e3eae21b9275d0bd91898365a59e1
Preisler <preisler.andras@gmail.com>
2023-04-22 17:41:27 +0200

    mAIN

--- lib/const/appConst.dart
+++ lib/const/appConst.dart
@@ -1,8 +1,9 @@
 class AppConstants {
-  //static const String Base_URL = 'https://helyettesites.petrik.hu/'; //pord
+
   static const String Base_URL = 'http://192.168.1.199:80'; //Preisler
   //static const String Base_URL = 'http://192.168.64.202:80'; //preisle mac
   //static const String Base_URL = 'http://192.168.100.14:80'; //Mate
+    //static const String Base_URL = 'https://helyettesites.petrik.hu/'; //pord
   static const String API_URL = '$Base_URL/api';
   static const String API_LOGIN = '$API_URL/login';
   static const String API_IKSZ = '$API_URL/iksz';
